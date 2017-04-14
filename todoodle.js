b878d8dfd299c5d17ea242a0efd6746d9d3e8e87
jacob <jacob.patrick.harris@gmail.com>
2017-04-14 10:50:02 -0400

    doing a bit of listing

--- todoodle.js
+++ todoodle.js
@@ -1,102 +1,102 @@
-//utility fxn, not directly related the app
-var getDate = function(){
-  var today = new Date();
-  var dd = today.getDate();
-  var mm = today.getMonth()+1; //January is 0!
-  var yyyy = today.getFullYear();
+// utility fxn, not directly related the app
+const getDate = function () {
+  const today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1; // January is 0!
+  const yyyy = today.getFullYear();
 
-  if(dd<10) {
-      dd='0'+dd
-  } 
+  if (dd < 10) {
+    dd = `0${dd}`;
+  }
 
-  if(mm<10) {
-      mm='0'+mm
-  } 
+  if (mm < 10) {
+    mm = `0${mm}`;
+  }
 
-  return mm+'/'+dd+'/'+yyyy;
+  return `${mm}/${dd}/${yyyy}`;
 };
 
 
 var todo = {
-  init : function(){
-    var addTodo = function() { 
+  init() {
+    const addTodo = function () {
       todo.makeNewTodo();
       todo.render();
     };
-    //attach click handler to the add-todo button
-    document.getElementById("input-todo")
-        .addEventListener("keyup", function(event) {
-        event.preventDefault();
-        if (event.keyCode == 13) {
+    // attach click handler to the add-todo button
+    document.getElementById('input-todo')
+        .addEventListener('keyup', (event) => {
+          event.preventDefault();
+          if (event.keyCode == 13) {
             addTodo();
-        }
-    });
-    document.getElementById("add-todo").onclick = addTodo;
+          }
+        });
+    document.getElementById('add-todo').onclick = addTodo;
 
-    //set an empty array to localstorage
-    if (localStorage.getItem('doItLater') === null){
+    // set an empty array to localstorage
+    if (localStorage.getItem('doItLater') === null) {
       console.log('no todos found, make empty list');
       localStorage.setItem('doItLater', '[]');
     }
-    //call render    
+    // call render
     todo.render();
   },
-  updateLocalStorage : function(arr){
+  updateLocalStorage(arr) {
     localStorage.setItem('doItLater', JSON.stringify(arr));
   },
-  retrieve : function(){
-   return JSON.parse(localStorage.getItem('doItLater')) ;
+  retrieve() {
+    return JSON.parse(localStorage.getItem('doItLater'));
   },
-  makeNewTodo: function(){
-    var currentList = this.retrieve();
+  makeNewTodo() {
+    const currentList = this.retrieve();
     currentList.push({
-      'name': document.getElementById('input-todo').value,
-      'created': getDate(),
-      'completed': false,
-      'details':''
+      name: document.getElementById('input-todo').value,
+      created: getDate(),
+      completed: false,
+      details: ''
     });
     this.updateLocalStorage(currentList);
 
-    //clear input value
+    // clear input value
     document.getElementById('input-todo').value = '';
   },
-  removeTodo: function(index){
-    var currentList = this.retrieve();
+  removeTodo(index) {
+    const currentList = this.retrieve();
     if (index > -1) {
       currentList.splice(index, 1);
     } else {
       console.log('error removing todo, index less than 0');
-    };
+    }
     this.updateLocalStorage(currentList);
     this.render();
   },
-  render: function(){
-    //get #todo-list, empty current contents
-    var list = document.getElementById('todo-list');
+  render() {
+    // get #todo-list, empty current contents
+    const list = document.getElementById('todo-list');
     while (list.firstChild) {
-        list.removeChild(list.firstChild);
+      list.removeChild(list.firstChild);
     }
 
-    //retrieve todos from localstorage
-    var todos = this.retrieve();
+    // retrieve todos from localstorage
+    const todos = this.retrieve();
 
-    //render them to the DOM
+    // render them to the DOM
     for (i = 0; i < todos.length; i++) {
-      var div = document.createElement('div');
-      div.appendChild(document.createTextNode(todos[i].name + ' ('  + todos[i].created + ')'));
+      const div = document.createElement('div');
+      div.appendChild(document.createTextNode(`${todos[i].name} (${todos[i].created})`));
       list.appendChild(div);
       div.classList.add('todo');
 
-      //create a complete button and attach a click handler to it
-      var checkbox = document.createElement('input');
+      // create a complete button and attach a click handler to it
+      const checkbox = document.createElement('input');
       checkbox.appendChild(document.createTextNode('COMPLETE'));
       checkbox.classList.add('pull-right');
       checkbox.setAttribute('data-todo', i);
       checkbox.setAttribute('type', 'checkbox');
 
       div.appendChild(checkbox);
-      checkbox.onclick = function() { 
-        var todoIndex = this.getAttribute('data-todo');
+      checkbox.onclick = function () {
+        const todoIndex = this.getAttribute('data-todo');
         todo.removeTodo(todoIndex);
       };
     }
@@ -104,5 +104,4 @@ var todo = {
 };
 
 todo.init();
-
 
